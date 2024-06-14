09330a0a153473ee5e9afa3246a364cd265eec07
Abhiraj412 <abhangparth@gmail.com>
2024-06-14 13:03:34 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import Signin from "./Signin.jsx";
 import Signup from "./Signup.jsx";
 import Appbar from "./Appbar.jsx";
 import AddCourse from "./AddCourse.jsx";
+import Courses from "./Courses";
+import Course from "./Course";
+import { useEffect, useState } from 'react';
+import axios from "axios";
+
 
 function App() {
 
@@ -11,14 +16,17 @@ function App() {
             height: "100vh",
             backgroundColor: "#eeeeee"}}
         >
-            <Router>
-                <Appbar />
-                <Routes>
-                    <Route path={"/addcourse"} element={<AddCourse />} />
-                    <Route path={"/signin"} element={<Signin />} />
-                    <Route path={"/signup"} element={<Signup />} />
-                </Routes>
-            </Router>
+                <Router>
+                    <Appbar />
+                    <Routes>
+                        <Route path={"/addcourse"} element={<AddCourse />} />
+                        <Route path={"/course/:courseId"} element={<Course />} />
+                        <Route path={"/courses"} element={<Courses />} />
+                        <Route path={"/signin"} element={<Signin />} />
+                        <Route path={"/signup"} element={<Signup />} />
+                    </Routes>
+                </Router>
+
         </div>
     );
 }
